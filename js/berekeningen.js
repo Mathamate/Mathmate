90352ca813c70f2e06c73fcb5536ee34f08171c2
Marieke van Rijn <6010946@mborijnland.nl>
2021-06-28 19:17:35 +0200

    Responsive gemaakt

--- js/berekeningen.js
+++ js/berekeningen.js
@@ -4,25 +4,32 @@
 {
         num1 = document.getElementById("firstNumber").value;
         num2 = document.getElementById("secondNumber").value;
-        document.getElementById("result").innerHTML = num1 * num2;
+        document.getElementById("result").innerHTML = "Het antwoord is : "+num1 * num2;
 }
 
 function divideBy() 
 { 
         num1 = document.getElementById("firstNumber").value;
         num2 = document.getElementById("secondNumber").value;
-        document.getElementById("result").innerHTML = num1 / num2;
+        document.getElementById("result").innerHTML = "Het antwoord is : "+num1 / num2;
 }
 function plus() 
 { 
         num1 = document.getElementById("firstNumber").value;
         num2 = document.getElementById("secondNumber").value;
         num3 = +num1 + +num2;
-        document.getElementById("result").innerHTML = num3;
+        document.getElementById("result").innerHTML = "Het antwoord is : "+num3;
 }
 function min() 
 { 
         num1 = document.getElementById("firstNumber").value;
         num2 = document.getElementById("secondNumber").value;
         document.getElementById("result").innerHTML = num1-num2;
+}
+
+// Machten
+function berekenMacht() {
+    var num1 = document.getElementById("macht").value;
+    var num2 = document.getElementById("macht2").value;
+  document.getElementById("demo").innerHTML ="Antwoord: " + Math.pow(num1, num2);
 }
